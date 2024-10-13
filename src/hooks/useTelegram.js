6c46f8f1fe7a3c6c24597e851b8d338b6d939e78
za5en <79802829+za5en@users.noreply.github.com>
2024-10-13 19:28:05 +0400

    test2

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -25,27 +25,27 @@ export function useTelegram() {
       if (typeof tg.initDataUnsafe.hash !== 'undefined' && tg.initDataUnsafe.hash !== null) {
         tokenString += `hash=${tg.initDataUnsafe.hash}&`
       }
-    //   if (typeof tg.initDataUnsafe.user !== 'undefined' && tg.initDataUnsafe.user !== null) {
-    //     tokenString += `user=${JSON.stringify(tg.initDataUnsafe.user)}&`
-    //   }
-    //   if (typeof tg.initDataUnsafe.receiver !== 'undefined' && tg.initDataUnsafe.receiver !== null) {
-    //     tokenString += `receiver=${JSON.stringify(tg.initDataUnsafe.receiver)}&`
-    //   }
-    //   if (typeof tg.initDataUnsafe.chat !== 'undefined' && tg.initDataUnsafe.chat !== null) {
-    //     tokenString += `chat=${JSON.stringify(tg.initDataUnsafe.chat)}&`
-    //   }
-    //   if (typeof tg.initDataUnsafe.chat_type !== 'undefined' && tg.initDataUnsafe.chat_type !== null) {
-    //     tokenString += `chat_type=${tg.initDataUnsafe.chat_type}&`
-    //   }
-    //   if (typeof tg.initDataUnsafe.chat_instance !== 'undefined' && tg.initDataUnsafe.chat_instance !== null) {
-    //     tokenString += `chat_instance=${tg.initDataUnsafe.chat_instance}&`
-    //   }
-    //   if (typeof tg.initDataUnsafe.start_param !== 'undefined' && tg.initDataUnsafe.start_param !== null) {
-    //     tokenString += `start_param=${tg.initDataUnsafe.start_param}&`
-    //   }
-    //   if (typeof tg.initDataUnsafe.can_send_after !== 'undefined' && tg.initDataUnsafe.can_send_after !== null) {
-    //     tokenString += `can_send_after=${tg.initDataUnsafe.can_send_after}`
-    //   }
+      if (typeof tg.initDataUnsafe.user !== 'undefined' && tg.initDataUnsafe.user !== null) {
+        tokenString += `user=${JSON.stringify(tg.initDataUnsafe.user)}&`
+      }
+      if (typeof tg.initDataUnsafe.receiver !== 'undefined' && tg.initDataUnsafe.receiver !== null) {
+        tokenString += `receiver=${JSON.stringify(tg.initDataUnsafe.receiver)}&`
+      }
+      if (typeof tg.initDataUnsafe.chat !== 'undefined' && tg.initDataUnsafe.chat !== null) {
+        tokenString += `chat=${JSON.stringify(tg.initDataUnsafe.chat)}&`
+      }
+      if (typeof tg.initDataUnsafe.chat_type !== 'undefined' && tg.initDataUnsafe.chat_type !== null) {
+        tokenString += `chat_type=${tg.initDataUnsafe.chat_type}&`
+      }
+      if (typeof tg.initDataUnsafe.chat_instance !== 'undefined' && tg.initDataUnsafe.chat_instance !== null) {
+        tokenString += `chat_instance=${tg.initDataUnsafe.chat_instance}&`
+      }
+      if (typeof tg.initDataUnsafe.start_param !== 'undefined' && tg.initDataUnsafe.start_param !== null) {
+        tokenString += `start_param=${tg.initDataUnsafe.start_param}&`
+      }
+      if (typeof tg.initDataUnsafe.can_send_after !== 'undefined' && tg.initDataUnsafe.can_send_after !== null) {
+        tokenString += `can_send_after=${tg.initDataUnsafe.can_send_after}`
+      }
       if (tokenString[tokenString.length - 1] === '&') {
         tokenString = tokenString.slice(0, -1);
       }
